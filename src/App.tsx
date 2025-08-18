a59b6d8ebe2781a896a40fc9f72f2ef3f1454854
stephanelias <stephane.elias229@gmail.com>
2025-08-18 22:29:20 +0200

    [DRD-18] ui : add prelineUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import "./index.css"
+async function loadPreline() {
+  return import('preline/dist/index.js')
+}
+
 function App() {
-  return <h1 className="text-neutral-500">Hello !</h1>
+  const location = useLocation()
+
+  useEffect(() => {
+    const initPreline = async () => {
+      await loadPreline()
+
+      if (window.HSStaticMethods && typeof window.HSStaticMethods.autoInit === 'function') {
+        window.HSStaticMethods.autoInit()
+      }
+    }
+
+    initPreline()
+  }, [location.pathname])
+
+  return (
+    <div className="container">
+      <h1 className="text-neutral-500">Hello</h1>
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import $ from 'jquery'
+import _ from 'lodash'
+import noUiSlider from 'nouislider'
+import 'datatables.net'
+import 'dropzone/dist/dropzone-min.js'
+import * as VanillaCalendarPro from 'vanilla-calendar-pro'
+import { BrowserRouter } from 'react-router-dom'
+
+window._ = _
+window.$ = $
+window.jQuery = $
+window.DataTable = $.fn.dataTable
+window.noUiSlider = noUiSlider
+window.VanillaCalendarPro = VanillaCalendarPro
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 )
