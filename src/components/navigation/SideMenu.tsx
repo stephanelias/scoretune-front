2ad036fda1bfef91203769bbd95141b8ebab60d4
stephanelias <stephane.elias229@gmail.com>
2026-01-31 19:56:45 +0100

    fix: refacto menu

--- src/components/navigation/SideMenu.tsx
+++ src/components/navigation/SideMenu.tsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
 
+import { useAuth } from '../../core/contexts/AuthContext'
+
 export default function SideMenu() {
+  const { user } = useAuth()
+
   return (
     <>
       <div className="mb-5 px-2 flex flex-col gap-y-5">
@@ -32,41 +36,37 @@ export default function SideMenu() {
               </span>
             </Link>
           </li>
-          <li>
-            <Link
-              className="group relative w-full flex items-center gap-1 py-1.5 px-2.5 relative text-sm text-gray-800 rounded-lg before:absolute before:inset-y-0 before:-start-2 before:rounded-e-full before:w-1 before:h-full hover:bg-gray-100/70 focus:outline-hidden focus:bg-gray-100/70 "
-              to="/auth/login"
-            >
-              <span className="-ms-[5px] flex shrink-0 justify-center items-center size-6">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="shrink-0 size-4 group-hover:scale-115 group-focus:scale-115 transition-transform duration-300"
-                >
-                  <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
-                  <polyline points="10 17 15 12 10 7" />
-                  <line x1="15" x2="3" y1="12" y2="12" />
-                </svg>
-              </span>
-              <span className="truncate hs-overlay-minified:opacity-0 transition-opacity duration-300">
-                Se connecter
-              </span>
-            </Link>
-          </li>
+          {!user && (
+            <li>
+              <Link
+                className="group relative w-full flex items-center gap-1 py-1.5 px-2.5 relative text-sm text-gray-800 rounded-lg before:absolute before:inset-y-0 before:-start-2 before:rounded-e-full before:w-1 before:h-full hover:bg-gray-100/70 focus:outline-hidden focus:bg-gray-100/70 "
+                to="/auth/login"
+              >
+                <span className="-ms-[5px] flex shrink-0 justify-center items-center size-6">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="shrink-0 size-4 group-hover:scale-115 group-focus:scale-115 transition-transform duration-300"
+                  >
+                    <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
+                    <polyline points="10 17 15 12 10 7" />
+                    <line x1="15" x2="3" y1="12" y2="12" />
+                  </svg>
+                </span>
+                <span className="truncate hs-overlay-minified:opacity-0 transition-opacity duration-300">
+                  Se connecter
+                </span>
+              </Link>
+            </li>
+          )}
         </ul>
-      </div>
-
-      <div className="hs-overlay-minified:opacity-0 transition-opacity duration-300 pb-4 px-2 size-full flex flex-col gap-y-5 overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300">
-        <div className="flex flex-col">
-          <span className="block ps-2.5 mb-2 text-sm text-gray-400">ADMIN</span>
-        </div>
       </div>
     </>
   )
